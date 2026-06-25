6c4019f57028606ee1b61b9e3265955ef02f42bf
salvaggio-mary <141263731+salvaggio-mary@users.noreply.github.com>
2026-06-25 23:40:52 +0000

    First Draft

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import re
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+txt = st.text_area(
+    "Copy from SEM starting at HAZARDOUS CHEMICALS POTENTIALLY ENCOUNTERED BY LABOR CATEGORY")
+
+#finding individual exposures
+pattern = r"Review details for this chemical \t(.*?) CAS"
+matches = re.findall(pattern, txt)
+
+st.write("Exposures:")
+#print list
+for i in matches:
+  st.write(i.strip())
